be06b7178795a7b0ba9102372b41bcdebd21c789
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2017-10-06 13:25:37 +0000

    Customize IOC start commands

--- 3-5/iocBoot/iocGalilDmc30017/st.cmd
+++ 3-5/iocBoot/iocGalilDmc30017/st.cmd
@@ -1,8 +1,5 @@
 #!../../bin/linux-x86_64/GalilDmc30017
 
-## You may have to change GalilDmc30017 to something else
-## everywhere it appears in this file
-
 < envPaths
 
 cd "${TOP}"
@@ -11,11 +8,8 @@ cd "${TOP}"
 dbLoadDatabase "dbd/GalilDmc30017.dbd"
 GalilDmc30017_registerRecordDeviceDriver pdbbase
 
-## Load record instances
-#dbLoadRecords("db/xxx.db","user=janitoHost")
-
 cd "${TOP}/iocBoot/${IOC}"
-iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=janitoHost"
+epicsEnvSet("IOC_APP_DIR", "${TOP}/GalilDmc30017App")
+
+iocInit
