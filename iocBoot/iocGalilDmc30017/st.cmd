93cf61c5fdedf1e82a88f68c7c820272e9ef7866
andrei_pereira <andrei.pereira@lnls.br>
2018-03-05 11:06:54 -0300

    Add iocBoot/ directory outside 3-5/ directory

---
+++ iocBoot/iocGalilDmc30017/st.cmd
@@ -0,0 +1,25 @@
+#!../../bin/linux-x86_64/GalilDmc30017
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/GalilDmc30017.dbd"
+GalilDmc30017_registerRecordDeviceDriver pdbbase
+
+cd "${TOP}/iocBoot/${IOC}"
+
+epicsEnvSet("IOC_APP_DIR", "${TOP}/galilDmc30017App")
+
+dbLoadTemplate("${IOC_APP_DIR}/Db/galil_motors.substitutions")
+dbLoadTemplate("${IOC_APP_DIR}/Db/galil_motor_extras.substitutions")
+dbLoadTemplate("${IOC_APP_DIR}/Db/galil_dmc_ctrl.substitutions")
+
+GalilCreateController("Galil", "${IPADDR}", 8)
+GalilCreateAxis("Galil","A",1,"",1)
+GalilStartController("Galil", "", 1, 0)
+
+iocInit
+
+dbpf "DMC01:A_AMPGAIN_CMD" "2"
