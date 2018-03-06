904176c3fbe96cd1c06e872541533034388718ec
andrei_pereira <andrei.pereira@lnls.br>
2018-03-06 15:53:43 -0300

    iocBoot/iocGalilDmc30017/runProcServ.sh: Change macros used

--- iocBoot/iocGalilDmc30017/runProcServ.sh
+++ iocBoot/iocGalilDmc30017/runProcServ.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Use defaults if not set
-if [ -z "${GALIL_DEVICE_TELNET_PORT}" ]; then
-   GALIL_DEVICE_TELNET_PORT=20000
+if [ -z "${DMC30017_DEVICE_TELNET_PORT}" ]; then
+   DMC30017_DEVICE_TELNET_PORT=20000
 fi
 
 # Run run*.sh scripts with procServ
-/usr/local/bin/procServ -f -n galil_dmc30017_${GALIL_INSTANCE} -i ^C^D ${GALIL_DEVICE_TELNET_PORT} ./runGalilDmc30017.sh "$@"
+/usr/local/bin/procServ -f -n galil_dmc30017_${DMC30017_INSTANCE} -i ^C^D ${DMC30017_DEVICE_TELNET_PORT} ./runGalilDmc30017.sh "$@"
