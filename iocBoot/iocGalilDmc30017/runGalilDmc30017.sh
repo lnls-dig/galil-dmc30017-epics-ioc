bcaeeb33f866db465ca75a78e4f5c190923f6dcc
andrei_pereira <andrei.pereira@lnls.br>
2018-03-06 14:18:43 -0300

    iocBoot/iocGalilDmc30017: Update startup scripts to follow standard

--- iocBoot/iocGalilDmc30017/runGalilDmc30017.sh
+++ iocBoot/iocGalilDmc30017/runGalilDmc30017.sh
@@ -1,49 +1,20 @@
 #!/bin/sh
 
-IOC_NAME="GalilDmc30017"
+# Source environment
+. ./checkEnv.sh
 
-IOC_BOOT_DIR="$(dirname "$0")"
-IOC_BIN_DIR="$IOC_BOOT_DIR/../../bin"
+# Parse command-line options
+. ./parseCMDOpts.sh "$@"
 
-if [ -n "$EPICS_HOST_ARCH" ]; then
-    IOC_BIN="../../bin/$EPICS_HOST_ARCH/$IOC_NAME"
-elif [ "$(ls "$IOC_BIN_DIR" | wc -l)" -eq 1 ]; then
-    HOST_ARCH_GUESS="$(ls "$IOC_BIN_DIR")"
-    IOC_BIN="../../bin/$HOST_ARCH_GUESS/$IOC_NAME"
-else
-    if [ -z "$(ls "$IOC_BIN_DIR")" ]; then
-        echo "No IOC binaries found Did you run make install in the root directory?" >&2
-    else
-        echo 'Multiple IOC binaries found. Please set the $EPICS_HOST_ARCH environment variable with the appropriate value.' >&2
-        echo "    Available options: $(ls "$IOC_BIN_DIR")" >&2
-    fi
-
-    exit 1
+# Check last command return status
+if [ $? -ne 0 ]; then
+        echo "Could not parse command-line options" >&2
+        exit 1
 fi
 
-if [ -z "$IPPORT" ]; then
-    IPPORT=5025
+if [ -z "$EPICS_CA_MAX_ARRAY_BYTES" ]; then
+    export EPICS_CA_MAX_ARRAY_BYTES="50000000"
 fi
-
-while [ "$#" -gt 0 ]; do
-    case "$1" in
-        "-P") P="$2" ;;
-        "-R") R="$2" ;;
-        "-i"|"--ip-addr") IPADDR="$2" ;;
-        "-p"|"--ip-port") IPPORT="$2" ;;
-        *) echo "Usage:" >&2
-            echo "  $0 -i IPADDR -p IPPORT [-P P_VAL] [-R R_VAL]" >&2
-            echo >&2
-            echo " Options:" >&2
-            echo "  -i or --ip-addr    Configure IP address to connect to device" >&2
-            echo "  -p or --ip-port    Configure IP port number to connect to device" >&2
-            echo "  -P                 Configure value of \$(P) macro" >&2
-            echo "  -R                 Configure value of \$(R) macro" >&2
-            exit 2
-    esac
-
-    shift 2
-done
 
 if [ -z "$IPADDR" ]; then
     echo "Device IP address not set. Please use the -i option or set \$IPADDR environment variable" >&2
--- iocBoot/iocGalilDmc30017/stGalilDmc30017.cmd
+++ iocBoot/iocGalilDmc30017/stGalilDmc30017.cmd
@@ -1,6 +1,10 @@
 #!../../bin/linux-x86_64/GalilDmc30017
 
 < envPaths
+< galil_dmc30017.config
+
+# Log solicited command/response traffic into galil_debug.txt
+epicsEnvSet("GALIL_DEBUG_FILE", "galil_debug.txt")
 
 cd "${TOP}"
 
@@ -12,14 +16,28 @@ cd "${TOP}/iocBoot/${IOC}"
 
 epicsEnvSet("IOC_APP_DIR", "${TOP}/galilDmc30017App")
 
-dbLoadTemplate("${IOC_APP_DIR}/Db/galil_motors.substitutions")
-dbLoadTemplate("${IOC_APP_DIR}/Db/galil_motor_extras.substitutions")
-dbLoadTemplate("${IOC_APP_DIR}/Db/galil_dmc_ctrl.substitutions")
+dbLoadRecords("$(GALIL)/GalilSup/Db/galil_motor.template", "P=$(P)$(R), M=A, PORT=Galil, ADDR=0, EGU=mm, DESC=, VELO=0.25,  VMAX=0.25, ACCL=2, BDST=0, BVEL=0, BACC=5, MRES=.000001975, SREV=1000, ERES=0.000625, PREC=5, DHLM=86.9, DLLM=0, OFF=0, UEIP=No, RTRY=0, NTM=YES, PCOF=0, ICOF=0, DCOF=0")
+
+dbLoadRecords("$(GALIL)/GalilSup/Db/galil_motor_extras.template", "P=$(P)$(R), M=A, PORT=Galil, ADDR=0, PREC=3, SCAN=Passive, MTRTYPE=2, MTRON=0, EGU=mm")
+
+dbLoadRecords("$(GALIL)/GalilSup/Db/galil_dmc_ctrl.template", "P=$(P)$(R), PORT=Galil, SCAN=Passive, DEFAULT_HOMETYPE=1, DEFAULT_LIMITTYPE=1, PREC=5")
+
+#dbLoadTemplate("${IOC_APP_DIR}/Db/galil_motors.substitutions")
+#dbLoadTemplate("${IOC_APP_DIR}/Db/galil_motor_extras.substitutions")
+#dbLoadTemplate("${IOC_APP_DIR}/Db/galil_dmc_ctrl.substitutions")
 
 GalilCreateController("Galil", "${IPADDR}", 8)
 GalilCreateAxis("Galil","A",1,"",1)
 GalilStartController("Galil", "", 1, 0)
 
+< save_restore.cmd
+
 iocInit
 
-dbpf "DMC01:A_AMPGAIN_CMD" "2"
+# Set controller gain to comply with motor rated current
+dbpf "DMC01:A_AMPGAIN_CMD" "1"
+
+# save things every 5 seconds
+create_monitor_set("auto_settings_galil_dmc30017.req", 5,"P=$(P)$(R)")
+# The following line is necessary because of the save file name used in save_restore.cmd
+set_savefile_name("auto_settings_galil_dmc30017.req", "auto_settings_${P}${R}.sav")
